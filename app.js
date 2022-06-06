d14f654ff9a85da0f47d8f1307dd68981e344c78
deinsteins <hidayaatdenii@gmail.com>
2022-06-06 14:44:49 +0700

    Setup Express web server and CORS Options

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 
 const corsOptions = {
     origin: 'http://localhost:3000',
 }
 
-app.use(cors(corsOptions));
+const base_url = "/api";
+const PORT = process.env.PORT || 3000;
+const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded( { extended: true } ));
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome to Barterify REST API'
-    })
+  res.json({
+      message: 'Welcome to Barterify REST API'
+  })
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Now listening on port: ${PORT}`);
 });
+
+
